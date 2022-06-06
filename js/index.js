a92a75e8329700c037f5f9413e2eec3da43d7244
StevenTran <trantrongtinnna3k55@gmail.com>
2022-06-06 17:26:48 +0700

    fixed span display none

--- js/index.js
+++ js/index.js
@@ -50,6 +50,7 @@ Staff.prototype.rankStaff = function (workHour) {
 // Không cần phải làm hàm position, bỏ value trong html sẽ cho ra nội dung của option
 let staffs = [];
 document.getElementById('btnThemNV').addEventListener('click', addStaff);
+
 function addStaff() {
   const id = document.getElementById('tknv').value;
   const name = document.getElementById('name').value;
@@ -59,6 +60,18 @@ function addStaff() {
   const basicSalary = +document.getElementById('luongCB').value;
   const position = document.getElementById('chucvu').value;
   const workHour = +document.getElementById('gioLam').value;
+  
+  let isValid = validation() ; 
+ 
+
+  if (!isValid) {
+    alert ('Vui lòng nhập vào các giá trị')
+    return
+  }
+
+  document.getElementById('tknv').disabled = false ; 
+  document.getElementById('btnThemNV').disabled = false; 
+
   let staff = new Staff(
     id,
     name,
@@ -72,6 +85,7 @@ function addStaff() {
   staffs.push(staff);
   display(staffs);
   resetForm();
+  document.getElementById('btnThemNV').attributes  = 
 }
 function display(staffs) {
   const tbodyEl = document.getElementById('tableDanhSach');
@@ -92,12 +106,15 @@ function display(staffs) {
       currency: 'VND',
     })}</td>
     <td>${staff.rankStaff(staff.workHour)}</td>
-    <button class = "btn btn-success" onclick = "selectStaff('${
-      staff.id
-    }')"> Cập Nhật </button>
-    <button class = "btn btn-danger" onclick = "deleteStaff('${
-      staff.id
-    }')"> Xóa </button>
+    <button 
+    class = "btn btn-success"   
+    data-toggle="modal"
+    data-target="#myModal"  
+    onclick = "selectStaff('${staff.id}')"> Cập Nhật </button>
+    <button 
+    class = "btn btn-danger "   
+  
+    onclick = "deleteStaff('${staff.id}')"> Xóa </button>
     </tr>
     `;
   }
@@ -116,19 +133,14 @@ function resetForm() {
 }
 
 function deleteStaff(staffId) {
-  let index = -1;
-  for (let i = 0; i < staffs.length; i++) {
-    if (staffs[i].id === staffId) {
-      index = i;
-      break;
-    }
-  }
+  let index = findStaff (staffId) 
   if (index !== -1) {
     staffs.splice(index, 1);
     display(staffs);
   }
 }
 document.getElementById('btnTimNV').addEventListener('click', searchStaff);
+
 
 function searchStaff() {
   let searchEl = document.getElementById('searchName').value;
@@ -143,5 +155,113 @@ function searchStaff() {
     }
   } 
   display(newStaffs);
+}
+function selectStaff (staffId) {
+ 
+  let index = findStaff (staffId) ;
+  let staff = staffs[index] ; 
+  document.getElementById('tknv').value = staff.id ;
+  document.getElementById('name').value = staff.name ;
+  document.getElementById('email').value = staff.email ;
+  document.getElementById('password').value = staff.password ;
+  document.getElementById('datepicker').value = staff.dayStart ;
+  document.getElementById('luongCB').value = staff.basicSalary ;
+  document.getElementById('chucvu').value = staff.position ;
+  document.getElementById('gioLam').value = staff.workHour ;
+
+  document.getElementById('tknv').disabled = true ; 
+  document.getElementById('btnThemNV').disabled = true ; 
+
+  
+}
+
+function findStaff (staffId) {
+  let index = -1 ; 
+  for ( let i = 0 ; i < staffs.length ; i++ ) {
+    if (staffs[i].id === staffId) {
+      index = i ; 
+      break
+    }
+  }
+  return index
+}
+function updateStaff() {
+  const id = document.getElementById('tknv').value;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const dayStart = document.getElementById('datepicker').value;
+  const basicSalary = +document.getElementById('luongCB').value;
+  const position = document.getElementById('chucvu').value;
+  const workHour = +document.getElementById('gioLam').value;
+  const staff = new Staff (id , name , email, password, dayStart, basicSalary, position, workHour) 
+  let index = findStaff(staff.id) ;
+  staffs[index] =  staff
+  display (staffs)
+  resetForm ()
+}
+
+function isRequired (value) {
+  if (!value ) {
+    return false
+  }
+  return true ;
+} 
+function validation () {
+  const id = document.getElementById('tknv').value;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const dayStart = document.getElementById('datepicker').value;
+  const basicSalary = +document.getElementById('luongCB').value;
+  const position = document.getElementById('chucvu').value;
+  const workHour = +document.getElementById('gioLam').value;
+  
+  let isValid = true ; 
+
+  if (!isRequired(id)) {
+    isValid = false
+    document.getElementById('tbTKNV').innerHTML = "Tài khoản Nhân Viên không được để trống"
+    document.getElementById('tbTKNV').style.display = "block"
+  }
+  if (!isRequired(name)) {
+    isValid = false
+    document.getElementById('tbTen').innerHTML = "Tên không được để trống"
+    document.getElementById('tbTen').style.display = "block"
+  }
+  if (!isRequired(email)) {
+    isValid = false
+    document.getElementById('tbEmail').innerHTML = "Email không được để trống"
+    document.getElementById('tbEmail').style.display = "block"
+  }
+  
+  if (!isRequired(password)) {
+    isValid = false
+    document.getElementById('tbMatKhau').innerHTML = "Mật khẩu không được để trống"
+    document.getElementById('tbMatKhau').style.display = "block"
+  }
+  
+  if (!isRequired(dayStart)) {
+    isValid = false
+    document.getElementById('tbNgay').innerHTML = "Ngày làm không được để trống"
+    document.getElementById('tbNgay').style.display = "block"
+  }
+  if (!isRequired(basicSalary)) {
+    isValid = false
+    document.getElementById('tbLuongCB').innerHTML = "Lương cơ bản không được để trống"
+    document.getElementById('tbLuongCB').style.display = "block"
+  }
+  if (!isRequired(position)) {
+    isValid = false
+    document.getElementById('tbChucVu').innerHTML = "Chức vụ không được để trống"
+    document.getElementById('tbChucVu').style.display = "block"
+  }
+  if (!isRequired(workHour)) {
+    isValid = false
+    document.getElementById('tbGioLam').style.display = "block"
+    document.getElementById('tbGioLam').innerHTML = "Giờ làm không được để trống"
+  }
+
+  return isValid ; 
 }
 
